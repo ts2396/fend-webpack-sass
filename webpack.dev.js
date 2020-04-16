5a889caaab622cefbf09a7fa4e57ae495e39f959
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-04-15 20:12:43 -0400

    commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,10 +15,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        }
         ]
     },
     plugins: [
